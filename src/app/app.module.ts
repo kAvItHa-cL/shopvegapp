4b6e282c039c17665b955c145e3e13f80cae1af2
kAvItHa-cL <kavithaln219@gmail.com>
2020-09-21 09:41:59 +0530

    5 files modified, 1 added- Menu Bar Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent} from './components/login/login.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule, FormsModule ],
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule,AngularMaterialModule ],
   declarations: [ AppComponent, HomeComponent,LoginComponent ],
   bootstrap:    [ AppComponent ],
   providers:[]
